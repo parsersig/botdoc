ad12b928ce9fd5d701217bd7e974d56f97a8aa96
parsersig <parsersig@gmail.com>
2025-05-19 15:35:46 +0500

    Update config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+// Основные настройки бота
+define('TELEGRAM_BOT_TOKEN', 'YOUR_BOT_TOKEN');
+define('ADMIN_ID', YOUR_ADMIN_ID); // Ваш ID в Telegram
+define('BOT_USERNAME', 'YourBotUsername'); // Без @
+define('CHANNEL_ID', null); // Опционально
