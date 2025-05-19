78b6f4042eef673810183d27b7f180ccd3005362
parsersig <parsersig@gmail.com>
2025-05-19 16:40:08 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,1 @@
-<?php
-define('TELEGRAM_BOT_TOKEN', 'your_bot_token_here'); // Replace with your BotFather token
-define('ADMIN_ID', 'your_admin_id_here'); // Replace with your Telegram user ID
-define('BOT_USERNAME', '@YourBotUsername'); // Replace with your bot's username, e.g., @MyBot
-// Optional: define('CHANNEL_ID', 'your_channel_id_here');
-?>
+
